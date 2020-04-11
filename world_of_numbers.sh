512ace66fdf89c2eba97bff789368fb5a1f5e6c9
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-10 22:44:23 -0500

    Adding solution for "World of numbers"

---
+++ world_of_numbers.sh
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem?h_r=next-challenge&h_v=zen
+
+read a
+read b
+echo $((a + b))
+echo $((a - b))
+echo $((a * b))
+echo $((a / b))
+
