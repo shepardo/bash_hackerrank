561d3d49fe26381a147334a9e9fe7b94b4582c71
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-15 13:58:11 -0500

    Adding solution for "Text processing head 1"

---
+++ text_processing_head1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-head-1/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+cat /dev/stdin | head -n 20 -
+
+# cat | cut -d$'\n' -f -20
