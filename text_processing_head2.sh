1639d904b0f7bf2d8f4f5ba2ac779eefc5263fe4
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-16 18:19:55 -0500

    Adding solution for "Text processing head 2".

---
+++ text_processing_head2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-head-2/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+head -c20 < /dev/stdin
