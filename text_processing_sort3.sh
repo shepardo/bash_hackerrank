3c3ff3d44594a4a93f76f1d4c5bd5829f4a8ae10
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-17 21:43:40 -0500

    Adding solution to "Text processing sort 3"

---
+++ text_processing_sort3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-sort-3/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+sort -k1 -n < /dev/stdin
