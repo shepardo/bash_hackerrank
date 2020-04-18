ffe7000c58c5443ec2edca50ba18a7135c94ad81
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-17 20:47:31 -0500

    Adding solution for "Text processing tail 1" problem.

---
+++ text_processing_tail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-tail-1/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+tail -n20 < /dev/stdin
