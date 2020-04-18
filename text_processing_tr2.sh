e6ad9b68d5b72574af9c6068b05523b151a12456
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-17 21:01:48 -0500

    Adding solutoin for "Text processing tr 2" problem.

---
+++ text_processing_tr2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-tr-2/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+
+tr -d "a-z" < /dev/stdin
+
