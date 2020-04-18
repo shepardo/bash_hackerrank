ba0a5ae52e500bc8976ddc56ef63a00b32757d8f
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-17 20:37:30 -0500

    Adding solution for "Middle of text file" problem.

---
+++ middle_of_text_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-in-linux---the-middle-of-a-text-file/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+cut -f12-22 -d$'\n' < /dev/stdin
