4482da0fd59b1ceb70fc31a5363cea505f4f0578
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-17 21:19:02 -0500

    Adding solution for "Text processing tr 3"

---
+++ text_processing_tr3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-tr-3/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+tr -s ' ' ' ' < /dev/stdin
