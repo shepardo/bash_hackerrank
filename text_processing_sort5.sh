00625445046197f3ce1919da9d666464171895bf
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-18 23:30:58 -0500

    Adding solution for "Text processing sort 5"

---
+++ text_processing_sort5.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-sort-5/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+sort -k2 -n -r -t $'\t' < /dev/stdin
