3a723b4e36111cbe371bd3692a0a1431ec9c0b02
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-18 23:34:10 -0500

    Adding solution for "Text processing sort 7"

---
+++ text_processing_sort7.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-sort-7/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+sort -k2 -n -r -t $'|' < /dev/stdin
