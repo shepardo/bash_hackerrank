876a25ba29672a14812ac9c00303c84f906a8506
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-18 23:27:19 -0500

    Adding solution for "Text processing sort 4"

---
+++ text_processing_sort4.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-sort-4/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+sort -k1 -n -r < /dev/stdin
