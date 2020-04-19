e9cfc702406cbd3bb3ed5fc3c7860cd302c823f0
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-18 23:58:52 -0500

    Adding missing commnts to "Text processing uniq 3"

--- text_processing_uniq3.sh
+++ text_processing_uniq3.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/text-processing-in-linux-the-uniq-command-3/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
 uniq -c -i < /dev/stdin | sed -e 's/^[ \t]*//'
