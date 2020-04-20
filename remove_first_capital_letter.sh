6a52b55155e95c145f3775e4f48c77d9d90d72fd
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-20 00:20:38 -0500

    Adding solution for "Remove first capital letter"

---
+++ remove_first_capital_letter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://www.hackerrank.com/challenges/bash-tutorials-remove-the-first-capital-letter-from-each-array-element/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+i=0
+while IFS= read line
+do
+    if [[ ${line:0:1} =~ [A-Z] ]]; then
+        line=".${line:1}"
+    fi
+    a[$i]=$line
+    i=$i+1
+done
+echo ${a[@]}
