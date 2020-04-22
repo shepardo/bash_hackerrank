78788fd58805555e2179fcabecbb1e330809869c
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-21 23:32:07 -0500

    Adding missing -w option to grep1 solution.

--- grep1.sh
+++ grep1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-4/problem
 
-grep -i -e the -e that -e 'then' -e those < /dev/stdin
+grep -i -w -e the -e that -e 'then' -e those < /dev/stdin
