982ed712c51b2979391233255bcc2b86b2a38433
Fernando Gonzalez Sanchez <1685248+shepardo@users.noreply.github.com>
2020-04-27 23:05:59 -0500

    Adding other samples for grep1.

--- grep1.sh
+++ grep1.sh
@@ -2,3 +2,11 @@
 # https://www.hackerrank.com/challenges/text-processing-in-linux-the-grep-command-1/problem
 
 grep -w 'the'
+
+
+#grep -wie 'the' < /dev/stdin
+
+#echo "`cat`" |grep -w 'the'
+
+#grep -w 'the'
+#grep '\bthe\b'
